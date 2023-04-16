0de6eb307b622efc3d58968e95d4f120552ae942
Glauber Holanda <glauberholanda10@gmail.com>
2023-04-15 21:23:15 -0300

    refactor: change headers

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const API = axios.create({
   baseURL: 'https://api.cartola.globo.com/',
   headers: {
-    'origin': 'https://cartola.globo.com'
+    'Access-Control-Allow-Origin' : '*',
   }
 })
